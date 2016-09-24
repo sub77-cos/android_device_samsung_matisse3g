36d976905f90e56c5ee5f2d65c168f10ef449030
Aditya Upreti <aupreti72@gmail.com>
2016-09-24 09:45:12 +0530

    Add Gello browser

--- device.mk
+++ device.mk
@@ -24,6 +24,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Gello
+PRODUCT_PACKAGES += \
+    Gello
+
 #GPS
 PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
